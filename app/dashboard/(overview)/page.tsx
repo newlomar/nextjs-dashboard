c9a76183511824977dbd1db1fd6d1e69a3ca2a7a
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-11-17 13:44:36 -0300

    better import

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,8 +4,10 @@ import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
-import { LatestInvoicesSkeleton } from "@/app/ui/skeletons";
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from "@/app/ui/skeletons";
 
 export default async function Page() {
   const {
