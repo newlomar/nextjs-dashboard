fe2911859d747599cb94ad320d9eaed3d5c1eefe
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-11-20 17:17:59 -0300

    adding FormSchema

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,14 @@
 
 import { z } from "zod";
 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
 export async function createInvoice(formData: FormData) {
   const rawFormData = {
     customerId: formData.get("customerId"),
