8b9361c3225a949aeb37d5411493a1f72a31abee
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-11-20 17:16:43 -0300

    importing zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,12 @@
 "use server";
 
+import { z } from "zod";
+
 export async function createInvoice(formData: FormData) {
   const rawFormData = {
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   };
-  // Test it out:
   console.log(rawFormData);
 }
