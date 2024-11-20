b9a2bd4afd4121e6b4df9248e6873b4e8ace8e87
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-11-20 17:26:58 -0300

    importing fetch customers

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchCustomers } from "@/app/lib/data";
+import { fetchCustomers, fetchCustomers } from "@/app/lib/data";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
