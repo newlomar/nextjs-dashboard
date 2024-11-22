db7eb3a8d76668dd1dd2632eac3aee4c2ee54233
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-11-22 00:44:24 -0300

    getting invoice and customers

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -5,6 +5,11 @@ import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
   return (
     <main>
       <Breadcrumbs
