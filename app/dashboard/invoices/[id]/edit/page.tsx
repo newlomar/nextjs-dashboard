f1c33aa0647546b65a95593a447aabf6a2a1be73
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-11-22 00:43:17 -0300

    getting id from params

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,7 +4,7 @@ import { fetchCustomers } from "@/app/lib/data";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
-
+  const id = params.id;
   return (
     <main>
       <Breadcrumbs
