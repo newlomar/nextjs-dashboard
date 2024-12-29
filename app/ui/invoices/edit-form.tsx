a59795143bd2c39bc8e5dc20056e30d11b6d2a75
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-12-29 19:52:41 -0300

    import useActionState

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useActionState } from "react";
 import { updateInvoice } from "@/app/lib/actions";
 import { CustomerField, InvoiceForm } from "@/app/lib/definitions";
 import {
