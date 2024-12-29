c327cfa95a1aa064537e023c25116f5457200c23
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-12-29 17:01:56 -0300

    throwing error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -65,6 +65,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error("Failed to Delete Invoice");
+
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     expirePath("/dashboard/invoices");
