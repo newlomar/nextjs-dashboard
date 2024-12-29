a57979d4023114f65aeb23b2934c86c333082593
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2024-12-29 17:28:03 -0300

    running notFound fucntion if invoice is not found

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -11,6 +11,11 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
