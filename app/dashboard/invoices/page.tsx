3caaffe1906e8e243ed2b0e6adffeaa0a50c9297
Newton Lomar <40671286+newlomar@users.noreply.github.com>
2025-01-05 08:36:41 -0300

    updating invoice title to fits template

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -9,7 +9,7 @@ import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
 
 export const metadata: Metadata = {
-  title: "Invoices | Acme Dashboard",
+  title: "Invoices",
 };
 
 export default async function Page(props: {
